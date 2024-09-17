989d49ab68a0f91c1e79b99e441910de15f7edfc
Dai182003 <daitoncamp18@gmail.com>
2024-09-16 19:52:52 -0500

    Update anim.js

--- anim.js
+++ anim.js
@@ -66,8 +66,8 @@ function ocultarTitulo() {
     "fadeOut 3s ease-in-out forwards"; /* Duración y función de temporización de la desaparición */
   setTimeout(function () {
     titulo.style.display = "none";
-  }, 3000); // Espera 3 segundos antes de ocultar completamente
+  }, 1000); // Espera 3 segundos antes de ocultar completamente
 }
 
 // Llama a la función después de 216 segundos (216,000 milisegundos)
-setTimeout(ocultarTitulo, 216000);
+setTimeout(ocultarTitulo, 21600);
