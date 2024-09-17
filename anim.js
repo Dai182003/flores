579f118758aa48a7d473f10444dc04e1273e8136
Dai182003 <daitoncamp18@gmail.com>
2024-09-16 19:12:54 -0500

    Update anim.js

--- anim.js
+++ anim.js
@@ -5,7 +5,7 @@ var lyrics = document.querySelector("#lyrics");
 // Array de objetos que contiene cada línea y su tiempo de aparición en segundos
 var lyricsData = [
   { text: "Él la estaba esperando con una flor amarilla", time: 6 },
-  { text: "The whisper of birds", time: 18 },
+  { text: "Ella lo estaba soñando con la luz en su pupila", time: 6 },
   { text: "Lonely before the sun cried", time: 27 },
   { text: "Fell from the sky", time: 32 },
   { text: "Like water drops", time: 33 },
