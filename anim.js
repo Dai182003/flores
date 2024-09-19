5c35ad689df0fb5309aa9b8b2855b5e1678b249a
Dai182003 <daitoncamp18@gmail.com>
2024-09-19 10:51:50 -0500

    Update anim.js

--- anim.js
+++ anim.js
@@ -38,10 +38,10 @@ var lyricsData = [
   { text: "Está dicho que es un hecho", time: 184 },
   { text: "No la pierdas, no hay derecho", time: 186 },
   { text: "No te olvides que la vida", time: 189 },
-  { text: "Casi nunca está dormida", time: 195 },
+  { text: "Casi nunca está dormida", time: 194 },
   { text: "Ella sabía que él sabía", time: 197 },
-  { text: "Él sabía, ella sabía", time: 198 },
-  { text: "Que él sabía, ella sabía", time: 199 },
+  { text: "Él sabía, ella sabía", time: 199 },
+  { text: "Que él sabía, ella sabía", time: 201 },
   { text: "Y se olvidaron de sus flores amarillas", time: 206 }
 ];
 
